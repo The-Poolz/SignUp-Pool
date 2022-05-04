0e88a6baf4b8a1159dfdad372f9d0637a8a28cf6
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-05-04 16:06:50 +0300

    removed onlyOwner modifier, added pool owner

--- contracts/PoolControl.sol
+++ contracts/PoolControl.sol
@@ -11,6 +11,7 @@ contract PoolControl is Manageable {
     event PoolDeactivated(uint256 PoolId);
     
     mapping(uint256 => bool) public isPoolActive;
+    mapping(uint256 => address) public poolOwner;
     uint256 public PoolsCount;
 
     modifier validatePoolId(uint256 _poolId) {
@@ -18,20 +19,23 @@ contract PoolControl is Manageable {
         _;
     }
 
-    function CreateNewPool() external onlyOwner {
+    function CreateNewPool() external {
         isPoolActive[PoolsCount] = true;
         emit NewPoolActivated(PoolsCount);
         PoolsCount = SafeMath.add(PoolsCount, 1);
+        poolOwner[PoolsCount] = msg.sender;
     }
 
-    function ActivatePool(uint256 _poolId) external onlyOwner validatePoolId(_poolId) {
+    function ActivatePool(uint256 _poolId) external validatePoolId(_poolId) {
         require(!isPoolActive[_poolId], "Pool is Already Active");
+        require(poolOwner[PoolsCount] == msg.sender, "Invalid Pool owner");
         isPoolActive[_poolId] = true;
         emit PoolActivated(_poolId);
     }
 
-    function DeactivatePool(uint256 _poolId) external onlyOwner validatePoolId(_poolId) {
+    function DeactivatePool(uint256 _poolId) external validatePoolId(_poolId) {
         require(isPoolActive[_poolId], "Pool is Already Inactive");
+        require(poolOwner[PoolsCount] == msg.sender, "Invalid Pool owner");
         isPoolActive[_poolId] = false;
         emit PoolDeactivated(_poolId);
     }
